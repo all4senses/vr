547879d18bf811119fa24041ba96793aaba1550b
all4senses <all4senses@gmail.com>
2013-04-27 15:09:26 +0300

    Quote block select box

--- modules/_Structure/selectmenu/js/jquery.ui.selectmenu/jquery.ui.selectmenu.js
+++ modules/_Structure/selectmenu/js/jquery.ui.selectmenu/jquery.ui.selectmenu.js
@@ -857,6 +857,7 @@ $.widget("ui.selectmenu", {
 			//var _offset = "0 " + ( this.list.offset().top  - selected.offset().top - ( this.newelement.outerHeight() + selected.outerHeight() ) / 2);
       var _offset = "0 " + ( this.list.offset().top  - selected.offset().top - ( this.newelement.outerHeight() + selected.outerHeight() ) / 2 - 20);
 		}
+    console.log(_offset);
 		this.listWrap
 			.removeAttr('style')
 			.zIndex( this.element.zIndex() + 1 )
