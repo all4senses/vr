23ce5bf057678e39487e202f45b940e1f09333dd
all4senses <all4senses@gmail.com>
2013-04-27 14:46:00 +0300

    Quote block select box

--- modules/_VR/vr_blocks/js/vr_requestquote_v1.js
+++ modules/_VR/vr_blocks/js/vr_requestquote_v1.js
@@ -3,10 +3,10 @@
   Drupal.behaviors.vr_requestquote_block_v1 = {
     attach: function (context, settings) {
        
-        $('#requestQuoteFormWrapper select').selectmenu({
-          //style:'popup', 
-          maxHeight: 300
-  			});
+//        $('#requestQuoteFormWrapper select').selectmenu({
+//          //style:'popup', 
+//          maxHeight: 300
+//  			});
 //        
 //        $('.ui-selectmenu').click(function(){
 //          console.log('click!');
