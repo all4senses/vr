e7014eb0d3232bc73d89ccee7dc484595e3b327b
all4senses <all4senses@gmail.com>
2013-05-13 21:49:02 +0300

    VR provider page styling

--- themes/vr/templates/node--provider.tpl.php
+++ themes/vr/templates/node--provider.tpl.php
@@ -216,7 +216,7 @@
 
                       <?php 
                         $stars_overall = theme('vr_misc_fivestar_static', array('rating' => $node->vr_rating_overall * 20, 'stars' => 5, 'tag' => 'overall', 'widget' => array('name' => 'stars', 'css' => 'stars.css')));
-                        echo '<div class="rating_overall">Rating: ' . $node->vr_rating_overall . ' out of 5' . $stars_overall . '</div>';
+                        echo '<div class="rating_overall"><span>Rating: ' . $node->vr_rating_overall . ' out of 5</span>' . $stars_overall . '</div>';
                         echo '<div class="voters"><div class="title">' . 'Number of Reviews' . ':</div><div class="count" property="v:count">' . $node->vr_voters . '</div></div>'; 
                         echo '<div class="recommend"><div class="title">Would recommend: </div><div class="data">' . $node->vr_recommend . '% of Users' . '</div></div>'; 
                       ?>
