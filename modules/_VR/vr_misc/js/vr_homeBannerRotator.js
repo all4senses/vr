96295ecd11cc16d718b33c4e474cb454509002f2
all4senses <all4senses@gmail.com>
2013-05-16 23:38:21 +0300

    VR Home Content Rotator

--- modules/_VR/vr_misc/js/vr_homeBannerRotator.js
+++ modules/_VR/vr_misc/js/vr_homeBannerRotator.js
@@ -3,7 +3,8 @@
   Drupal.behaviors.vr_homeBannerRotator = {
     attach: function (context, settings) {
        
-       $("#featured > ul").tabs({fx:{opacity: "toggle"}}).tabs("rotate", 5000, true);
+       //$("#featured > ul").tabs({fx:{opacity: "toggle"}}).tabs("rotate", 5000, true);
+       $("#featured").tabs({fx:{opacity: "toggle"}}).tabs("rotate", 5000, true);
        
        console.log('teeest');
        
