ac40099f4812c311e2eac89f4ac90f1a847b6630
all4senses <all4senses@gmail.com>
2013-05-20 18:22:06 +0300

    Home banners rotator

--- modules/_VR/vr_misc/js/vr_homeBannerRotator.js
+++ modules/_VR/vr_misc/js/vr_homeBannerRotator.js
@@ -4,7 +4,7 @@
     attach: function (context, settings) {
        
       //$("#featured > ul").tabs({fx:{opacity: "toggle"}}).tabs("rotate", 5000, true); // for a Version of UI > 1.9
-      $("#featured").tabs({fx:{opacity: "toggle"}}).tabs("rotate", 5000, true);
+      $("#featured").tabs({fx:{opacity: "toggle"}}).tabs("rotate", 30000, true);
        
       // Pause on hover.
       $("#featured").hover(  
